19939a14d1bd5e9f2d7f533e4c9d86f5b0e3778a
Akram Hussein <akramhussein@gmail.com>
2016-04-22 12:27:42 +0100

    Replace offending characters (non-alphanumeric,_ or -) with -

--- Pod/Classes/SEGFacebookAppEventsIntegration.m
+++ Pod/Classes/SEGFacebookAppEventsIntegration.m
@@ -10,24 +10,29 @@
 {
     if (self = [super init]) {
         self.settings = settings;
-        
+
         NSString *appId = [settings objectForKey:@"appId"];
         [FBSDKSettings setAppID:appId];
     }
+
+    NSMutableCharacterSet *charactersToKeep = [[NSCharacterSet alphanumericCharacterSet] mutableCopy];
+    [charactersToKeep addCharactersInString:@"_-"];
+    self.charactersToReplace = [[charactersToKeep copy] invertedSet];
+
     return self;
 }
 
 + (NSNumber *)extractRevenue:(NSDictionary *)dictionary withKey:(NSString *)revenueKey
 {
     id revenueProperty = nil;
-    
+
     for (NSString *key in dictionary.allKeys) {
         if ([key caseInsensitiveCompare:revenueKey] == NSOrderedSame) {
             revenueProperty = dictionary[key];
             break;
         }
     }
-    
+
     if (revenueProperty) {
         if ([revenueProperty isKindOfClass:[NSString class]]) {
             // Format the revenue.
@@ -44,7 +49,7 @@
 + (NSString *)extractCurrency:(NSDictionary *)dictionary withKey:(NSString *)currencyKey
 {
     id currencyProperty = nil;
-    
+
     for (NSString *key in dictionary.allKeys) {
         if ([key caseInsensitiveCompare:currencyKey] == NSOrderedSame) {
             currencyProperty = dictionary[key];
@@ -60,23 +65,24 @@
 {
     [[NSOperationQueue mainQueue] addOperationWithBlock:^ {
         // FB Event Names must be <= 40 characters
-        NSString *truncatedEvent = [payload.event substringToIndex: MIN(40, [payload.event length])];
-        
+        NSString *sanitizedEvent = [payload.event substringToIndex: MIN(40, [payload.event length])];
+        sanitizedEvent = [[sanitizedEvent componentsSeparatedByCharactersInSet: self.charactersToReplace] componentsJoinedByString:@"-"];
+
         // Revenue & currency tracking
         NSNumber *revenue = [SEGFacebookAppEventsIntegration extractRevenue:payload.properties withKey:@"revenue"];
         NSString *currency = [SEGFacebookAppEventsIntegration extractCurrency:payload.properties withKey:@"currency"];
         if (revenue) {
             [FBSDKAppEvents logPurchase:[revenue doubleValue] currency:currency];
-        
+
             // Custom event
             NSMutableDictionary *properties = [payload.properties mutableCopy];
             [properties setObject:currency forKey:FBSDKAppEventParameterNameCurrency];
-            [FBSDKAppEvents logEvent:truncatedEvent
+            [FBSDKAppEvents logEvent:sanitizedEvent
                             valueToSum:[revenue doubleValue]
                             parameters:properties];
         }
         else {
-            [FBSDKAppEvents logEvent:truncatedEvent
+            [FBSDKAppEvents logEvent:sanitizedEvent
                             parameters:payload.properties];
         }
     }];
@@ -87,11 +93,13 @@
     [[NSOperationQueue mainQueue] addOperationWithBlock:^ {
         // FB Event Names must be <= 40 characters
         // 'Viewed' and 'Screen' with spaces take up 14
-        NSString *truncatedEvent = [payload.name substringToIndex: MIN(26, [payload.name length])];
-        NSString *event = [[NSString alloc] initWithFormat:@"Viewed %@ Screen", truncatedEvent];
+        NSString *sanitizedEvent = [payload.name substringToIndex: MIN(26, [payload.name length])];
+        sanitizedEvent = [[sanitizedEvent componentsSeparatedByCharactersInSet: self.charactersToReplace] componentsJoinedByString:@"-"];
+
+        NSString *event = [[NSString alloc] initWithFormat:@"Viewed %@ Screen", sanitizedEvent];
         [FBSDKAppEvents logEvent:event];
     }];
-    
+
 }
 
 #pragma mark - Callbacks for app state changes
