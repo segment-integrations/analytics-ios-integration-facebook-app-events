2170b55714e7f321b2a16fd5debd5273931590fe
Gabriel P Samson <gabriel.samson@segment.com>
2020-07-21 15:23:42 -0700

    Implement data processing overrides

--- Pod/Classes/SEGFacebookAppEventsIntegration.h
+++ Pod/Classes/SEGFacebookAppEventsIntegration.h
@@ -4,7 +4,10 @@
 @interface SEGFacebookAppEventsIntegration : NSObject<SEGIntegration>
 
 @property(nonatomic, strong) NSDictionary *settings;
+@property(nonatomic, strong) NSArray<NSString *> *dataProcessingOptions;
+@property(nonatomic) int *dataProcessingCountry;
+@property(nonatomic) int *dataProcessingState;
 
-- (id)initWithSettings:(NSDictionary *)settings;
+- (id)initWithSettings:(NSDictionary *)settings dataProcessingOptions:(NSArray<NSString *> *)options dataProcessingCountry:(int *)country dataProcessingState:(int *)state;
 
 @end
